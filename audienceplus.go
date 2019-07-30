8b5871fa0e21834f13332238caea2cd3c6990cea
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-07-30 02:30:45 -0700

    Indent 4 spaces

--- audienceplus.go
+++ audienceplus.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
-	"net/http"
-	"io/ioutil"
-	"os"
-	"log"
-	"encoding/json"
-	"encoding/csv"
+    "fmt"
+    "net/url"
+    "net/http"
+    "io/ioutil"
+    "os"
+    "log"
+    "encoding/json"
+    "encoding/csv"
 )
 
 type searchListResponse struct {
-  Kind string `json:"kind"`
-  NextPageToken  string `json:"nextPageToken"`
-  Items []item
+    Kind string `json:"kind"`
+    NextPageToken  string `json:"nextPageToken"`
+    Items []item
 }
 
 type item struct {
-	Kind string `json:"kind"`
-	Snippet snippet
+    Kind string `json:"kind"`
+    Snippet snippet
 }
 
 type snippet struct {
-	PublishedAt string `json:"publishedAt"`
+    PublishedAt string `json:"publishedAt"`
 }
 
 func main() {
-	u, err := url.Parse("https://www.googleapis.com/youtube/v3/search")
-	if err != nil {
-		log.Fatal(err)
-	}
-	v := url.Values{}
-	v.Set("key", os.Getenv("YT_API_KEY"))
-	v.Add("part", "snippet")
-	v.Add("type", "video")
-	v.Add("maxResults", "2")
-	v.Add("order", "date")
-	v.Add("channelId", os.Args[1])
-	u.RawQuery = v.Encode()
-	fmt.Printf("%s\n", u.String())
+    u, err := url.Parse("https://www.googleapis.com/youtube/v3/search")
+    if err != nil {
+        log.Fatal(err)
+    }
+    v := url.Values{}
+    v.Set("key", os.Getenv("YT_API_KEY"))
+    v.Add("part", "snippet")
+    v.Add("type", "video")
+    v.Add("maxResults", "50")
+    v.Add("order", "date")
+    v.Add("channelId", os.Args[1])
+    u.RawQuery = v.Encode()
+    fmt.Printf("%s\n", u.String())
 
-	resp, err := http.Get(u.String())
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
+    resp, err := http.Get(u.String())
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
 
-  fmt.Println("Response status:", resp.Status)
+    fmt.Println("Response status:", resp.Status)
 
-	body, err := ioutil.ReadAll(resp.Body)
-	// fmt.Println(string(body))
+    body, err := ioutil.ReadAll(resp.Body)
+    // fmt.Println(string(body))
 
-	s := string(body)
-	bs := []byte(s)
+    s := string(body)
+    bs := []byte(s)
 
-	var searchList = searchListResponse{}
-  err = json.Unmarshal(bs, &searchList)
-  if err != nil {
+    var searchList = searchListResponse{}
+    err = json.Unmarshal(bs, &searchList)
+    if err != nil {
     fmt.Println(err)
-  }
-  // fmt.Println(searchList)
+    }
+    // fmt.Println(searchList)
 
-	w := csv.NewWriter(os.Stdout)
+    w := csv.NewWriter(os.Stdout)
 
-	for _, item := range searchList.Items {
-		record := []string{ item.Kind, item.Snippet.PublishedAt }
-		if err := w.Write(record); err != nil {
-			log.Fatalln("error writing record to csv:", err)
-		}
-	}
+    for _, item := range searchList.Items {
+        record := []string{ item.Kind, item.Snippet.PublishedAt }
+        if err := w.Write(record); err != nil {
+            log.Fatalln("error writing record to csv:", err)
+        }
+    }
 
-	w.Flush()
+    w.Flush()
 
-	if err := w.Error(); err != nil {
-		log.Fatal(err)
-	}
+    if err := w.Error(); err != nil {
+        log.Fatal(err)
+    }
 }
