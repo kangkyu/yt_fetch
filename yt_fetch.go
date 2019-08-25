75e1441efcb2cebd93cc8a735ee0590521fdac55
Kang-Kyu Lee <kangkyu1111@gmail.com>
2019-08-24 22:35:43 -0700

    Refactor: more func

--- yt_fetch.go
+++ yt_fetch.go
@@ -64,26 +64,12 @@ func main() {
         log.Fatalln("error writing record to csv:", err)
     }
 
-    u, err := url.Parse("https://www.googleapis.com/youtube/v3/search")
-    if err != nil {
-        log.Fatal(err)
-    }
-    v := url.Values{}
-    v.Set("key", os.Getenv("YT_API_KEY"))
-    v.Add("part", "snippet")
-    v.Add("type", "video")
-    v.Add("maxResults", "50")
-    v.Add("order", "date")
-    v.Add("channelId", os.Args[1])
-    u.RawQuery = v.Encode()
+    u := searchURL("")
+    nextPageToken, itemCount := videosFromURL(u, w)
 
-    nextPageToken, itemCount := videosFromURL(u.String(), w)
     for {
-        q := u.Query()
-        q.Set("pageToken", nextPageToken)
-        u.RawQuery = q.Encode()
-
-        nextPageToken, itemCount = videosFromURL(u.String(), w)
+        u = searchURL(nextPageToken)
+        nextPageToken, itemCount = videosFromURL(u, w)
         if len(nextPageToken) == 0 || itemCount == 0 {
             break
         }
@@ -96,10 +82,10 @@ func main() {
     }
 }
 
-func videosFromURL(uuu string, w *csv.Writer) (string, int) {
-    fmt.Printf("%s\n", uuu)
+func videosFromURL(uuu *url.URL, w *csv.Writer) (string, int) {
+    fmt.Printf("%s\n", uuu.String())
 
-    resp, err := http.Get(uuu)
+    resp, err := http.Get(uuu.String())
     if err != nil {
         log.Fatal(err)
     }
@@ -119,18 +105,7 @@ func videosFromURL(uuu string, w *csv.Writer) (string, int) {
         return searchList.NextPageToken, 0
     }
 
-    videoIDString := videoIDString(searchList.Items)
-
-    u3, err := url.Parse("https://www.googleapis.com/youtube/v3/videos")
-    if err != nil {
-        log.Fatal(err)
-    }
-    v3 := url.Values{}
-    v3.Set("key", os.Getenv("YT_API_KEY"))
-    v3.Add("part", "snippet,statistics")
-    v3.Add("id", videoIDString)
-    u3.RawQuery = v3.Encode()
-
+    u3 := videosURLFromVideoIds(searchList)
     fmt.Printf("%s\n", u3.String())
 
     resp3, err := http.Get(u3.String())
@@ -149,24 +124,81 @@ func videosFromURL(uuu string, w *csv.Writer) (string, int) {
         fmt.Println(err)
     }
 
+    videoList.print(w)
+
+    return searchList.NextPageToken, len(searchList.Items)
+}
+
+func searchURL(nextPageToken string) *url.URL {
+
+    u, err := url.Parse("https://www.googleapis.com/youtube/v3/search")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    v := url.Values{}
+    v.Set("key", os.Getenv("YT_API_KEY"))
+    v.Add("part", "snippet")
+    v.Add("type", "video")
+    v.Add("maxResults", "50")
+    v.Add("order", "date")
+    v.Add("channelId", os.Args[1])
+
+    if len(nextPageToken) != 0 {
+        v.Set("pageToken", nextPageToken)
+    }
+
+    u.RawQuery = v.Encode()
+
+    return u
+}
+
+
+func videosURLFromVideoIds(searchList searchListResponse) *url.URL {
+
+    videoIDString := strings.Join(*searchList.videoIDs(), ",")
+
+    u3, err := url.Parse("https://www.googleapis.com/youtube/v3/videos")
+    if err != nil {
+        log.Fatal(err)
+    }
+    v3 := url.Values{}
+    v3.Set("key", os.Getenv("YT_API_KEY"))
+    v3.Add("part", "snippet,statistics")
+    v3.Add("id", videoIDString)
+    u3.RawQuery = v3.Encode()
+
+    return u3
+}
+
+func (videoList videoListResponse) print(w *csv.Writer) {
+
     for _, item := range videoList.Items {
         parsedTime, err := time.Parse("2006-01-02T15:04:05Z07:00", item.Snippet.PublishedAt)
         if err != nil {
             fmt.Println(err)
         }
-        record := []string{item.Kind, parsedTime.Local().Format("2006-01-02"), item.Snippet.ChannelID, item.ID, item.Statistics.ViewCount}
+        record := []string{
+            item.Kind,
+            parsedTime.Local().Format("2006-01-02"),
+            item.Snippet.ChannelID,
+            item.ID, item.Statistics.ViewCount}
         if err := w.Write(record); err != nil {
             log.Fatalln("error writing record to csv:", err)
         }
     }
-
-    return searchList.NextPageToken, len(searchList.Items)
 }
 
-func videoIDString(items []searchItem) string {
-    ids := make([]string, 0, len(items))
-    for _, item := range items {
-        ids = append(ids, item.ID.VideoID)
+func (searchList searchListResponse) videoIDs() *[]string {
+    // items.collect{|item| item.video_id}.compact.uniq.join(",")
+    keys := make(map[string]bool)
+    ids := []string{}
+    for _, item := range searchList.Items {
+        id := item.ID.VideoID
+        if _, value := keys[id]; !value {
+            keys[id] = true
+            ids = append(ids, id)
+        }
     }
-    return strings.Join(ids, ",")
+    return &ids
 }
