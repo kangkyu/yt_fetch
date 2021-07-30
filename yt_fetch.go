caf3bb56da965c8495916e5c872400aca887e3aa
kangkyu <kangkyu1111@gmail.com>
2021-07-30 09:02:53 -0700

    Refactor: easier to show columns

--- yt_fetch.go
+++ yt_fetch.go
@@ -42,7 +42,7 @@ func fetchHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/csv")
 	w.Header().Set("Content-Disposition", "attachment;filename=videosof"+channelID+".csv")
-	
+
 	if err := generateCSV(w, channelID); err != nil {
 		fmt.Fprint(w, "could not generate CSV:\n")
 		fmt.Fprint(w, err.Error())
@@ -167,22 +167,14 @@ func videosURL(videoIDs []string) *url.URL {
 }
 
 func printHeaderRow(cw *csv.Writer) error {
+	header := make([]string, 0, len(fields))
 
-	header := []string{
-		"kind",
-		"publishedAt",
-		"channelId",
-		"channelTitle",
-		"id",
-		"title",
-		"categoryId",
-		"viewCount",
-		"likeCount",
-		"dislikeCount",
-		"favoriteCount",
-		"commentCount",
-		"privacyStatus",
-		"duration",
+	for i := 0; i < len(fields); i++ {
+		for field, value := range fields {
+			if value == i {
+				header = append(header, field)
+			}
+		}
 	}
 	if err := cw.Write(header); err != nil {
 		return fmt.Errorf("error writing header to csv: %v", err)
@@ -190,33 +182,59 @@ func printHeaderRow(cw *csv.Writer) error {
 	return nil
 }
 
-func printVideos(cw *csv.Writer, videoList videoListResponse) error {
+var fields = map[string]int{
+	"kind": 0,
+	"publishedAt": 1,
+	"channelId": 2,
+	"channelTitle": 3,
+	"id": 4,
+	"title": 5,
+	"categoryId": 6,
+	"viewCount": 7,
+	"likeCount": 8,
+	"dislikeCount": 9,
+	"favoriteCount": 10,
+	"commentCount": 11,
+	"privacyStatus": 12,
+	"duration": 13,
+}
 
+func printVideos(cw *csv.Writer, videoList videoListResponse) error {
 	row := make([]string, 14)
+
 	for _, item := range videoList.Items {
-		parsedTime, err := time.Parse("2006-01-02T15:04:05Z07:00", item.Snippet.PublishedAt)
+
+		row[fields["kind"]] = item.Kind
+		publishedOn, err := parseDate(item.Snippet.PublishedAt)
 		if err != nil {
-			return err
+			return fmt.Errorf("error parsing publishedAt (can it be?)", err)
 		}
-		row[0] = item.Kind
-		row[1] = parsedTime.Local().Format("2006-01-02")
-		row[2] = item.Snippet.ChannelID
-		row[3] = item.Snippet.ChannelTitle
-		row[4] = item.ID
-		row[5] = item.Snippet.Title
-		row[6] = item.Snippet.CategoryID
-		row[7] = item.Statistics.ViewCount
-		row[8] = item.Statistics.LikeCount
-		row[9] = item.Statistics.DislikeCount
-		row[10] = item.Statistics.FavoriteCount
-		row[11] = item.Statistics.CommentCount
-		row[12] = item.Status.PrivacyStatus
-		row[13] = item.ContentDetails.Duration
+		row[fields["publishedAt"]] = publishedOn
+		row[fields["channelId"]] = item.Snippet.ChannelID
+		row[fields["channelTitle"]] = item.Snippet.ChannelTitle
+		row[fields["id"]] = item.ID
+		row[fields["title"]] = item.Snippet.Title
+		row[fields["categoryId"]] = item.Snippet.CategoryID
+		row[fields["viewCount"]] = item.Statistics.ViewCount
+		row[fields["likeCount"]] = item.Statistics.LikeCount
+		row[fields["dislikeCount"]] = item.Statistics.DislikeCount
+		row[fields["favoriteCount"]] = item.Statistics.FavoriteCount
+		row[fields["commentCount"]] = item.Statistics.CommentCount
+		row[fields["privacyStatus"]] = item.Status.PrivacyStatus
+		row[fields["duration"]] = item.ContentDetails.Duration
 		if err := cw.Write(row); err != nil {
 			return fmt.Errorf("error writing a row to csv: %v", err)
 		}
 	}
 	return nil
+}
+
+func parseDate(ts string) (string, error) {
+	t, err := time.Parse("2006-01-02T15:04:05Z07:00", ts)
+	if err != nil {
+		return "", err
+	}
+	return t.Local().Format("2006-01-02"), nil
 }
 
 func (searchList searchListResponse) videoIDs() []string {
